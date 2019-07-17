96a5f5b8fb6e0ff0ccbcfb6e2a30c56a6d7c715e
Pontsho MOGWERE <pmogwere@c5r5s7.wethinkcode.co.za>
2019-07-17 18:29:20 +0200

    Mods not printing

--- ft_rdirec.c
+++ ft_rdirec.c
@@ -5,12 +5,12 @@ t_list      *newlst(char *str)
     t_list	*elem;
 	t_list	*adds;
 
-	if (!(elem = ft_lstnew(str, sizeof(str))))
+	if (!(elem = ft_lstnew(str, ft_strlen(str))))
     {
         printf("Error");
 		return (0);
     }
-	if (!(adds = ft_lstnew(str, sizeof(str))))
+	if (!(adds = ft_lstnew(str, ft_strlen(str))))
     {
         printf("Error");
 		return (0);
@@ -19,26 +19,30 @@ t_list      *newlst(char *str)
     return (elem);
 }
 
-void  dirlist(char *str)
+void  dirlist(const char *str, const char *string)
 {
     DIR *d;
     struct dirent *dir;
     t_list *ss;
     char *s;
 
-    d = opendir(str);
+    if (!(d = opendir(str)))
+    {
+        printf("Not dir\n");
+        return ;
+    }
     ss = ft_lstnew(0, 0);
     if (d)
     {
-        printf("%s\n", str);
+        //printf("%s\n", str);
         while ((dir = readdir(d)) != 0)
         {
             s = dir->d_name;
             if (s[0] == '.')
                 continue;
             if (dir->d_type == DT_DIR && ft_strcmp(s , ".") != 0 && ft_strcmp(s, "..") != 0)
-                ft_lstadd(&ss, newlst(dir->d_name));
-            printf("%s\t", dir->d_name);
+                ft_lstadd(&ss, newlst(s));
+            //printf("%s\t", dir->d_name);
         }
         closedir(d);
     }
@@ -46,12 +50,29 @@ void  dirlist(char *str)
         printf("\n\n");
     while (ss->next != 0)
     {
-        dirlist(ss->content);
+        printf("\n%s\n", string);
+        s = (char *)str;
+        if (ft_strcmp(str, "."))
+        {
+            s = ft_strcat(s, "/");
+            s = ft_strcat(s, ss->content);
+        }
+        else
+            s = ss->content;
+        dirlist((const char *)s, string);
+        //str = (char *)str2;
         ss = ss->next;
     }
 }
 
-int main(void) {
-    dirlist(".");
+int main(int argc, char **argv) {
+    if (argc == 1)
+        dirlist(".", "a");
+    else
+    {
+        (void)argc;
+        dirlist(argv[1], argv[1]);
+    }
+    printf("\n");
     return 0;
 }
