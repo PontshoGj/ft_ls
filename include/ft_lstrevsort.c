40afeec4cb924cc8b23f40c2117671f77d9ba5e5
Pontsho MOGWERE <pmogwere@c5r5s6.wethinkcode.co.za>
2019-08-26 13:28:22 +0200

    more norm

--- include/ft_lstrevsort.c
+++ include/ft_lstrevsort.c
@@ -1,28 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstrevsort.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmogwere <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/26 13:22:52 by pmogwere          #+#    #+#             */
+/*   Updated: 2019/08/26 13:24:46 by pmogwere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ls.h"
 
-void    ft_lstrevsort(t_list **lst)
+void		ft_lstrevsort(t_list **lst)
 {
-    t_list *lfirst;
-    t_list *lsecond;
-    int i;
-    int j;
+	t_list	*lfirst;
+	t_list	*lsecond;
+	int		i;
+	int		j;
 
-    lfirst = *lst;
-    lsecond = *lst;
-    i = 1;
-    j = 0;
-    while (lfirst != 0)
-    {
-        lsecond = *lst;
-        while (lsecond != 0)
-        {
+	lfirst = *lst;
+	lsecond = *lst;
+	i = 1;
+	j = 0;
+	while (lfirst != 0)
+	{
+		lsecond = *lst;
+		while (lsecond != 0)
+		{
 			j++;
-            if (ft_strcmp((char *)lfirst->content, (char *)lsecond->content) > 0)
+			if (ft_strcmp((char *)lfirst->content, \
+						(char *)lsecond->content) > 0)
 				ft_swaplst(lst, i, j);
-            lsecond = lsecond->next;
-        }
-        j = 0;
-        i++;
+			lsecond = lsecond->next;
+		}
+		j = 0;
+		i++;
 		lfirst = lfirst->next;
-    }
+	}
 }
--- include/ft_lstsort.c
+++ include/ft_lstsort.c
@@ -1,28 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmogwere <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/26 13:15:39 by pmogwere          #+#    #+#             */
+/*   Updated: 2019/08/26 13:20:37 by pmogwere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ls.h"
 
-void    ft_lstsort(t_list **lst)
+void		ft_lstsort(t_list **lst)
 {
-    t_list *lfirst;
-    t_list *lsecond;
-    int i;
-    int j;
+	t_list	*lfirst;
+	t_list	*lsecond;
+	int		i;
+	int		j;
 
-    lfirst = *lst;
-    lsecond = *lst;
-    i = 1;
-    j = 0;
-    while (lfirst->next != 0)
-    {
-        lsecond = *lst;
-        while (lsecond->next != 0)
-        {
+	lfirst = *lst;
+	lsecond = *lst;
+	i = 1;
+	j = 0;
+	while (lfirst->next != 0)
+	{
+		lsecond = *lst;
+		while (lsecond->next != 0)
+		{
 			j++;
-            if (ft_strcmp((char *)lfirst->content, (char *)lsecond->content) < 0)
+			if (ft_strcmp((char *)lfirst->content, \
+						(char *)lsecond->content) < 0)
 				ft_swaplst(lst, i, j);
-            lsecond = lsecond->next;
-        }
-        j = 0;
-        i++;
+			lsecond = lsecond->next;
+		}
+		j = 0;
+		i++;
 		lfirst = lfirst->next;
-    }
+	}
 }
