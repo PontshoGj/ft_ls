2341191c4c4296dfe3287d13d09804dedb7cf3bc
Pontsho MOGWERE <pmogwere@c4r6s8.wethinkcode.co.za>
2019-11-26 13:15:59 +0200

    done

--- include/ft_display_all.c
+++ include/ft_display_all.c
@@ -14,18 +14,8 @@
 
 void			ft_display_all(char *str, char *flag, char *path)
 {
-	static int	i;
-
-	if (!i)
-		i = 0;
-	else if (i == 5 && ft_strspn(flag, "R") == 0)
-	{
-		i = 0;
-		ft_printf("\n");
-	}
-	i++;
 	if (ft_strspn("l", flag) == 1)
 		ft_display_long(str, path);
 	else
-		ft_printf("%-24s", str);
+		ft_printf("%s\n", str);
 }
